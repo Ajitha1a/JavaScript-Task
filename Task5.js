46b8d4328dbcfc037719b4655c02c37081afc187
Ajitha1a <95858873+Ajitha1a@users.noreply.github.com>
2023-03-07 13:26:37 +0530

    Add files via upload

--- Task5.js
+++ Task5.js
@@ -2,5 +2,11 @@
 // Write a script that prompts the user for a number, and then creates an array with that number of elements, 
 // where each element is a random number between 1 and 100. The script should then output the array to the console.
 
-var user = prompt("hai");
-console.log("user",user);
+const userNumber = prompt("Enter the number");
+const arr = [];
+
+for (let i = 0; i < userNumber; i++) {
+  arr.push(Math.floor(Math.random() * 100));
+}
+
+console.log(arr);
