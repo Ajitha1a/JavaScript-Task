c9ca38ccb328e84554ba57acb88b3ff9ae3fe68f
Ajitha1a <95858873+Ajitha1a@users.noreply.github.com>
2023-03-07 10:51:57 +0530

    Add files via upload

---
+++ Task5.js
@@ -0,0 +1,6 @@
+// Task 5:
+// Write a script that prompts the user for a number, and then creates an array with that number of elements, 
+// where each element is a random number between 1 and 100. The script should then output the array to the console.
+
+var user = prompt("hai");
+console.log("user",user);
